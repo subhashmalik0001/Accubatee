1931dbe9a5e91a8660e3272e78d9c3be649fda9b
subhash <subhash@subhashs-MacBook-Air.local>
2025-12-10 22:55:35 +0530

    folderadd

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,46 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { useState } from 'react'
+import './App.css'
+import React from 'react'
+// Import your components (you'll create these)
+import Dashboard from './components/Dashboard'
+import Startups from './components/Startups'
+import Mentors from './components/Mentors'
+import Programs from './components/Programs'
+import Applications from './components/Applications'
+import Funding from './components/Funding'
+import Settings from './components/Settings'
+import Navigation from './components/Navigation'
+import Tools from './components/Tools'
+// import Auth from './components/Auth' // Removed in replacement
+import Login from './components/Login'
+import Signup from './components/Signup'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <Router>
+      <div className="min-h-screen bg-gray-50">
+        <Navigation />
+        <main className="p-6">
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/startups" element={<Startups />} />
+            <Route path="/mentors" element={<Mentors />} />
+            <Route path="/programs" element={<Programs />} />
+            <Route path="/applications" element={<Applications />} />
+            <Route path="/funding" element={<Funding />} />
+            <Route path="/settings" element={<Settings />} />
+            <Route path="/tools/*" element={<Tools />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+          </Routes>
+        </main>
+      </div>
+    </Router>
+  )
+}
+
+export default App
