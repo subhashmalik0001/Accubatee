a61e59fd69f540de9ff6bc0e19dd8f76e6a0e914
subhash <subhash@subhashs-MacBook-Air.local>
2025-12-10 22:56:08 +0530

    add

--- src/components/programs/sections/groups.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-export default function Groups() {
-    return (
-        <div className="space-y-2">
-            <h3 className="text-lg font-semibold text-gray-900">Groups</h3>
-            <p className="text-sm text-gray-600">Group configuration and membership.</p>
-        </div>
-    )
-}
